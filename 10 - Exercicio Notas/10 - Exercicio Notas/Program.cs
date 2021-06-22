d216ff3ddfe12fb037f19d0934fe0c56b9f68d70
Andriel Cardoso <57081914+AndrielCardoso@users.noreply.github.com>
2021-06-22 02:07:30 -0300

    Update Program.cs

--- 10 - Exercicio Notas/10 - Exercicio Notas/Program.cs
+++ 10 - Exercicio Notas/10 - Exercicio Notas/Program.cs
@@ -11,25 +11,26 @@ namespace _10___Exercicio_Notas
             // que o aluno obteve no ano juntamente com um texto explicativo. Caso a nota final do aluno seja inferior a 60, mostrar a mensagem "REPROVADO".
             // Todos os valores devem ter uma casa decimal.
             
-            //double nota1, nota2, notafinal;
+            double nota1, nota2, notafinal;
 
-            //Console.WriteLine("Escreva o valor da primeira nota: ");
-            //nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            //Console.WriteLine("Escreva o valor da segunda nota: ");
-            //nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Escreva o valor da primeira nota: ");
+            nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Escreva o valor da segunda nota: ");
+            nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            //notafinal = (nota1 + nota2) / 2;
+            notafinal = (nota1 + nota2) / 2;
 
-            //if (notafinal >= 6.0)
-            //{
-            //    Console.WriteLine("NOTA FINAL " + notafinal.ToString("F1"), CultureInfo.InvariantCulture + "\n" + "APROVADO");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("NOTA FINAL " + notafinal.ToString("F1"), CultureInfo.InvariantCulture + "\n" + "REPROVADO");
-            //}
+            if (notafinal >= 6.0)
+            {
+                Console.WriteLine("NOTA FINAL " + notafinal.ToString("F1"), CultureInfo.InvariantCulture + "\n" + "APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("NOTA FINAL " + notafinal.ToString("F1"), CultureInfo.InvariantCulture + "\n" + "REPROVADO");
+            }
 
-            // Bhaskara again
+            // Leia 3 valores de ponto flutuante e efetue o cálculo das raízes da equação de Bhaskara. Se não for possível calcular as raízes, 
+            // mostre a mensagem "Impossível Calcular", caso haja uma divisão por 0 ou raíz de um número negativo.
 
             double a, b, c, delta, x1, x2;
 
