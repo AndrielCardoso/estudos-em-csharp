4f3c03a23dd8bbeba7e191f40d40162f08555b5c
Andriel Cardoso <57081914+AndrielCardoso@users.noreply.github.com>
2021-06-24 02:23:46 -0300

    Estudos em C#

---
+++ 15- Crescente e Decrescente/15- Crescente e Decrescente/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _15__Crescente_e_Decrescente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Leia uma quantidade indeterminada de duplas de valores inteiros X e Y. Escreva para cada X e Y uma mensagem que indique se estes valores
+            // foram digitados em ordem crescente ou decrescente
+
+            int x, y;
+
+            String[] vet = Console.ReadLine().Split(' ');
+
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+
+            while (x != y)
+            {
+                if (x < y)
+                {
+                    Console.WriteLine("Crescente");
+          
+                }else
+                {
+                    Console.WriteLine("Decrescente");
+                }
+
+                vet = Console.ReadLine().Split(' ');
+
+                x = int.Parse(vet[0]);
+                y = int.Parse(vet[1]);
+            }
+
+        }
+    }
+}
---
+++ 17 - For/17 - For/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _17___For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, x, soma;
+
+            n = int.Parse(Console.ReadLine()); 
+
+            soma = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                x = int.Parse(Console.ReadLine());
+                soma = soma + x;
+                
+
+            }
+
+            Console.WriteLine("O resultado da soma da sequência é: " + soma);
+        }
+    }
+}
